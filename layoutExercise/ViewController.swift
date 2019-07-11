e8d779e0bf3863dd5e52ae32fc5231fa5495f9bc
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-10 20:45:49 -0400

    Initial Commit

---
+++ layoutExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layoutExercise
+//
+//  Created by Neeraj Prasher on 2019-07-10.
+//  Copyright © 2019 njprasher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
